08e9eb63079f09bcd1683634c8ae4bfa084d45b4
sufyan <mdsufyan7@gmail.com>
2023-04-12 06:16:00 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<div className="header">
+				<h1>Hello world</h1>
+				<nav className="menu">
+					<li>Home</li>
+					<li>About</li>
+					<li>Contact</li>
+					<li>Services</li>
+				</nav>
+			</div>
+			<aside>
+				<div className="sidebar">
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus commodi quo, ea qui obcaecati eveniet animi vitae assumenda blanditiis nulla repellendus, eum, necessitatibus corrupti ratione odio veritatis! Quis, blanditiis tenetur quas consequatur corporis eius voluptatibus libero corrupti obcaecati error minima aut earum molestias dicta modi veritatis! At, maiores? Unde, omnis!
+				</div>
+			</aside>
+			<main>
+				<div className="content">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem sint iste deserunt sequi saepe pariatur unde voluptatum, reiciendis animi obcaecati aliquam magnam sapiente similique vitae culpa commodi architecto iusto omnis eius maxime placeat! Voluptatibus, eos?
+				</div>
+			</main>
+			<footer>
+				<div className="footer">
+					Copyrights (c) 2022 All rights reserved.
+				</div>
+			</footer>
+		</>
+	);
 }
 
 export default App;
