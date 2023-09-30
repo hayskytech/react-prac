6eb7f7f0adccee58bb33bf061fba805b23004d55
sufyan <mdsufyan7@gmail.com>
2023-09-30 11:02:41 +0530

    delete functionality added

--- src/components/FB/Main.js
+++ src/components/FB/Main.js
@@ -1,60 +1,99 @@
 import React, { useEffect, useState } from 'react'
 import OTPLogin from '../OTPLogin'
-import { getDatabase, onValue, push, ref, set } from "firebase/database";
+import { getDatabase, onValue, push, ref, remove, set, update } from "firebase/database";
+import { Button, Container, Modal, Label, Input, List, Icon } from 'semantic-ui-react';
 
 export default function Main() {
   const [user, setUser] = useState(null)
   const [list, setlist] = useState([])
   const [item, setitem] = useState('')
-  // function writeUserData() {
-  //   const db = getDatabase();
-  //   set(ref(db, 'posts/' + user.uid), {
-  //     username: 'name',
-  //     email: 'emails',
-  //     profile_picture: 'imageUrl'
-  //   });
-  // }
+  const [dbox, setdbox] = useState(false)
+  const db = getDatabase();
+
   function additem() {
-    const db = getDatabase();
+    if (item === '') return
     const postListRef = ref(db, 'todos/' + user.uid);
     const newPostRef = push(postListRef);
     set(newPostRef, item);
+    setitem('')
   }
-
 
   useEffect(() => {
     if (user) {
-      const db = getDatabase();
       const starCountRef = ref(db, 'todos/' + user.uid);
       onValue(starCountRef, (snapshot) => {
         const data = snapshot.val()
         if (data) {
           setlist(data)
         } else {
-          console.log('data does not exists');
+          setlist([])
         }
       });
     }
   }, [user])
 
-
+  function deleteall() {
+    const starCountRef = ref(db, 'todos/' + user.uid);
+    remove(starCountRef)
+    setdbox(false)
+  }
+  function deletethis(key) {
+    const starCountRef = ref(db, 'todos/' + user.uid + '/' + key);
+    remove(starCountRef)
+  }
   return (
     <div>
-      <OTPLogin user={user} setUser={setUser} />
-      {/* <button onClick={addpost}>OK</button> */}
-      <hr />
-      Add Item:
-      <input type="text" value={item} onChange={(e) => { setitem(e.target.value) }} />
-      <button onClick={additem}>ADD</button>
-      <h2>Todo List</h2>
-      <ul>
-        {
-          Object.values(list).map((item) => {
-            return (<li>{item}</li>)
-          })
-        }
-      </ul>
+      <Container>
+        <OTPLogin user={user} setUser={setUser} />
 
+        <hr />
+        <Button color='red' onClick={() => { setdbox(true) }}>Delete All</Button>
+        <hr />
+
+        <Input placeholder="write something..." labelPosition='right' type='text' value={item} onChange={(e) => { setitem(e.target.value) }}>
+          <Label color='grey'>Item</Label>
+          <input />
+          <Label icon color='green' onClick={additem}><Icon name='plus' /></Label>
+        </Input>
+
+        <Modal
+          closeIcon
+          basic
+          size='small'
+          open={dbox}
+          closeOnDimmerClick={true}
+          onOpen={() => { setdbox(true) }}
+          onClose={() => { setdbox(false) }}
+        >
+          <Modal.Header>
+            <h3>Confirm Delete All?</h3>
+            <Button color='grey' onClick={() => { setdbox(false) }}>No</Button>
+            <Button color='red' onClick={deleteall}>Yes</Button>
+
+          </Modal.Header>
+          {/* <Modal.Actions> */}
+          {/* </Modal.Actions> */}
+        </Modal>
+
+
+        <h2>Todo List</h2>
+        <List divided verticalAlign='middle'>
+          {
+            Object.entries(list).map((item) => {
+              return (
+                <List.Item key={item[0]}>
+                  <List.Content floated='right'>
+                    <Button icon color='red' onClick={() => { deletethis(item[0]) }}>
+                      <Icon name='trash' />
+                    </Button>
+                  </List.Content>
+                  <List.Header>{item[1]}</List.Header>
+                </List.Item>
+              )
+            })
+          }
+        </List>
+      </Container>
     </div>
   )
 }
